d032775d734e352256061ed21e5022c04b564b66
kmozdzen <kmozdzen2@onet.pl>
2021-10-13 16:44:48 +0200

    "done2"

--- simpleCalc/src/main.cpp
+++ simpleCalc/src/main.cpp
@@ -4,6 +4,42 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "czesc"<<endl;
+    //simple calculator
+    int choice = stoi(argv[1]);
+    switch (choice) {
+        case 1:
+            if(argc == 4){ // nazwa + wybor + 2 parametry
+                cout << add(stoi(argv[2]), stoi(argv[3]));
+            }
+            else{
+                cout << "Zla ilosc parametrow"<<endl;
+                help();
+            }
+            break;
+        case 2:
+            if(argc == 4) { // nazwa + wybor + 2 parametry
+                cout << subtract(stoi(argv[2]), stoi(argv[3]));
+            }
+            else{
+                cout << "Zla ilosc parametrow"<<endl;
+                help();
+            }
+            break;
+        case 3:
+            if(argc == 6){ // nazwa + wybor + 4 parametry
+                cout << volume(stoi(argv[2]), stoi(argv[3]), stoi(argv[4]), stoi(argv[5]));
+            }
+            else{
+                cout << "Zla ilosc parametrow"<<endl;
+                help();
+            }
+            break;
+        case 4:
+            help();
+            break;
+        default:
+            cout << "Blad, musisz podac wartosc 1-4" << endl;
+    }
+
     return 0;
 }
