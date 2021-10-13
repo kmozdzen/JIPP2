19f776a1b38e1192336c560ed2a868aa11ec8768
kmozdzen <kmozdzen2@onet.pl>
2021-10-13 16:01:33 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/zadanie_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie_lab2.dir/main.cpp.o"
+  "libzadanie_lab2.dll.a"
+  "zadanie_lab2.exe"
+  "zadanie_lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zadanie_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
