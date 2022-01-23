5334ef041d44f224d53a48038241734db21da2ac
kmozdzen <kmozdzen2@onet.pl>
2022-01-23 18:41:15 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/lab12.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab12.dir/main.cpp.o"
+  "lab12.exe"
+  "lab12.pdb"
+  "liblab12.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
